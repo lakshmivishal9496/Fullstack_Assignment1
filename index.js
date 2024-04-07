ca6244acda0d49c2d1586a1aa51e58dfd9f657c2
lakshmi vishal <lakshmivishal9496@gmail.com>
2024-04-07 20:21:30 +0200

    Edited index.js to serve static(using express middleware) and fetched from api/recipes and rendered the database entries in frontend ui with help of table where each document content takes seperate rows

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { connectDB } from './db.js';
 import recipeRoutes from './routes/recipeRoutes.js';
 
-import cors from 'cors';
-import dotenv from 'dotenv';
+// Setup __dirname in ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(express.static('static')); // Serve static files
+
 app.use('/api/recipes', recipeRoutes);
+
+app.get('/home', (req, res) => {
+    console.log('Serving recipes.html from:', path.join(__dirname, 'static', 'recipes.html'));
+    res.sendFile(path.join(__dirname, 'static', 'recipes.html'));
+  });
+  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
